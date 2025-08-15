b7774bbf3156d9bcff29c2cc649a26636012c01c
Harsh Sharma <itisharshsharma18@gmail.com>
2025-08-15 12:57:45 +0530

    1. Revised the conditions in isQueueEmpty function. 2. Added some helpful comments explaining the code. 3. Added print statements to display what is being enqueued and dequeued.

--- DSA-In-C/Queue-using-array.c
+++ DSA-In-C/Queue-using-array.c
@@ -8,12 +8,17 @@ Queue implementation by Arrays by LazyAxe
 
 int Queue[MAX_SIZE];
 
+/*Intializing the front and rear to -1 to indicate the Queue is not initalized.*/
 int front=-1;
 int rear=-1;
 
 
 bool isQueueFull()
 {
+    /*
+        A condition to check if the Queue is full by checking if the rear==MAX_SIZE-1 i.e. Because the array's indexing is from 0 to n-1 meaning it's one behind.
+        If it was rear==MAX_SIZE, The index would be out of range.
+    */
     if(rear==MAX_SIZE-1)
     {
         return true;
@@ -26,7 +31,7 @@ bool isQueueFull()
 
 bool isQueueEmpty()
 {
-    if(front>rear||rear==-1)
+    if(front==-1 && rear==-1)
     {
         return true;
     }
@@ -35,6 +40,7 @@ bool isQueueEmpty()
         return false;
     }
 }
+
 void EnQueue(int InputValue)
 {
     if (isQueueFull()) 
@@ -42,7 +48,6 @@ void EnQueue(int InputValue)
         printf("\nQueue is full, can't EnQueue %d.\n", InputValue);
         return;
     }
-
     if (isQueueEmpty())
     {
         front = rear = 0;
@@ -53,6 +58,7 @@ void EnQueue(int InputValue)
     }
 
     Queue[rear] = InputValue;
+    printf("\nEnQueued %d\n", Queue[rear]);   
 }
 
 
@@ -61,14 +67,21 @@ void DeQueue()
     if(isQueueEmpty())
     {
         printf("\n Queue is empty.\n");
+        return;
     }
     else if(front==rear)
     {
+    /*
+        The front should only be equal to rear, if its the the first element.
+        Then the DeQueue should "remove" all the elements.
+    */
+        printf("\nDeQueued %d\n", Queue[front]);
         front=rear=-1;
     }
     else
     {
         front++;
+        printf("\nDeQueued %d\n", Queue[front]);
     }
 }
 
@@ -76,7 +89,10 @@ void display()
 {
     if(isQueueEmpty())
     {
+        printf("\n---------------------------------------\n");
         printf("\nQueue is empty, no display.\n");
+        printf("\n---------------------------------------\n");
+
     }
     else
     {
@@ -85,7 +101,7 @@ void display()
         {
             printf("%d ",Queue[i]);
         }
-        printf("\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n");
+        printf("\n---------------------------------------\n");
     }
 }
 
